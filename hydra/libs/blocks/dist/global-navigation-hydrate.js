b8e73de6210a8148078636cc8116fefdeade3f99
Rishiithaaa <157449216+Rishiithaaa@users.noreply.github.com>
2025-06-11 12:06:10 +0530

    work

--- hydra/libs/blocks/dist/global-navigation-hydrate.js
+++ hydra/libs/blocks/dist/global-navigation-hydrate.js
@@ -188,7 +188,7 @@ import {
   setAsyncDropdownCount,
   branchBannerLoadCheck,
   getBranchBannerInfo
-} from './utilities/utilities.js';
+} from 'libs/blocks/global-navigation/utilities/utilities.js';
 const hydrationToken = "global-navigation/global-navigation.js";
 const x=document.querySelector('header').getAttribute('data-feds');
 /**
