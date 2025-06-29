916750d1cb8b6f21728c6b5cbf433fc565ff5229
Rishiithaaa <157449216+Rishiithaaa@users.noreply.github.com>
2025-06-29 23:20:34 +0530

    check-makeTabActive

--- libs/blocks/dist/global-navigation-hydrate.js
+++ libs/blocks/dist/global-navigation-hydrate.js
@@ -153,6 +153,26 @@ class GnavHydrate extends Gnav {
     });
     return this.ready;
   };
+  makeTabActive = (popup) => {
+      const tabbuttons = popup.querySelectorAll('.global-navigation .tabs button');
+      const tabpanels = popup.querySelectorAll('.global-navigation .tab-content [role="tabpanel"]');
+      closeAllTabs(tabbuttons, tabpanels);
+      const { origin, pathname } = window.location;
+      const url = `${origin}${pathname}`;
+      setTimeout(() => {
+        const activeLink = [
+          ...popup.querySelectorAll('a:not([data-modal-hash])'),
+        ].find((el) => (el.href === url || el.href.startsWith(`${url}?`) || el.href.startsWith(`${url}#`)));
+        const tabIndex = activeLink ? +activeLink.parentNode.id : 0;
+        const selectTab = popup.querySelectorAll('.tab')[tabIndex];
+        const daallTab = selectTab.getAttribute('daa-ll');
+        selectTab.setAttribute('daa-ll', `${daallTab.replace('click', 'open')}`);
+        selectTab?.click();
+        selectTab.setAttribute('daa-ll', `${daallTab.replace('open', 'click')}`);
+        selectTab?.focus();
+      }, 100);
+    };
+
 }
 
 import {
